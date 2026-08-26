744193d36db3e5e916dbdf422a7fc83706eda6e2
abubakarsultan <127768101+abubakarsultan@users.noreply.github.com>
2026-08-26 12:38:24 +0500

    20

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import './glass-ui.css';
 import { AuthProvider } from '@/components/AuthProvider';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
